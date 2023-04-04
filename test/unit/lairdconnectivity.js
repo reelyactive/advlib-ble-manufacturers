f351bec4042a58c686bf3836d33480a3a65b1dda
jeffyactive <jeff@reelyactive.com>
2023-04-04 17:11:58 -0400

    Added BT510 record events

--- test/unit/lairdconnectivity.js
+++ test/unit/lairdconnectivity.js
@@ -9,11 +9,41 @@ const assert = require ('assert');
 
 
 // Input data for the scenario
+const INPUT_DATA_TEMPERATURE = '0100000000006655443322110101001e621562ce04000000';
+const INPUT_DATA_MAGNET = '0100000000006655443322110201001e6215620000000000';
+const INPUT_DATA_MOVEMENT = '0100000000006655443322110301001e6215620000000000';
 const INPUT_DATA_BATTERY_VOLTAGE = '0100000000006655443322110c01001e621562050d000000';
 const INPUT_DATA_TEMPERATURE_3 = '0100000000006655443322111401001e6215620000a84100';
 
 // Expected outputs for the scenario
 const EXPECTED_DATA_INVALID_INPUT = null;
+const EXPECTED_DATA_TEMPERATURE = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    networkId: 0,
+    recordNumber: 1,
+    temperature: 12.3,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_MAGNET = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    isContactDetected: [ true ],
+    networkId: 0,
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_MOVEMENT = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    isMotionDetected: [ true ],
+    networkId: 0,
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
 const EXPECTED_DATA_BATTERY_VOLTAGE = {
     batteryVoltage: 3.333,
     deviceIds: [ "112233445566/3" ],
@@ -42,6 +72,24 @@ describe('lairdconnectivity', function() {
     assert.deepEqual(manufacturer.process(), EXPECTED_DATA_INVALID_INPUT);
   });
 
+  // Test the process function with temperature data
+  it('should handle BT510 temperature data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_TEMPERATURE),
+                     EXPECTED_DATA_TEMPERATURE);
+  });
+
+  // Test the process function with magnet data
+  it('should handle magnet data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_MAGNET),
+                     EXPECTED_DATA_MAGNET);
+  });
+
+  // Test the process function with movement data
+  it('should handle movement data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_MOVEMENT),
+                     EXPECTED_DATA_MOVEMENT);
+  });
+
   // Test the process function with battery voltage data
   it('should handle battery voltage data', function() {
     assert.deepEqual(manufacturer.process(INPUT_DATA_BATTERY_VOLTAGE),
@@ -49,7 +97,7 @@ describe('lairdconnectivity', function() {
   });
 
   // Test the process function with temperature data
-  it('should handle temperature data', function() {
+  it('should handle BT610 temperature data', function() {
     assert.deepEqual(manufacturer.process(INPUT_DATA_TEMPERATURE_3),
                      EXPECTED_DATA_TEMPERATURE_3);
   });
