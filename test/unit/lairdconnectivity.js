af7a139f0c5c0653da4b262aa54947d0fac3f55d
jeffyactive <jeff@reelyactive.com>
2023-04-04 20:43:58 -0400

    Implemented remaining BT610 data records

--- test/unit/lairdconnectivity.js
+++ test/unit/lairdconnectivity.js
@@ -14,6 +14,11 @@ const INPUT_DATA_MAGNET = '0100000000006655443322110201001e6215620000000000';
 const INPUT_DATA_MOVEMENT = '0100000000006655443322110301001e6215620000000000';
 const INPUT_DATA_BATTERY_VOLTAGE = '0100000000006655443322110c01001e621562050d000000';
 const INPUT_DATA_TEMPERATURE_3 = '0100000000006655443322111401001e6215620000a84100';
+const INPUT_DATA_VOLTAGE_2 = '0100000000006655443322111701001e62156200e4404600';
+const INPUT_DATA_CURRENT_4 = '0100000000006655443322111d01001e6215620000f64200';
+const INPUT_DATA_ULTRASONIC = '0100000000006655443322111e01001e6215620080264400';
+const INPUT_DATA_PRESSURE_1 = '0100000000006655443322111f01001e6215620000044200';
+const INPUT_DATA_TAMPER = '0100000000006655443322112601001e6215620100000000';
 const INPUT_DATA_CONTACT_TRACER =
 '81ffffff000066554433221100001e621562000130ce0000';
 
@@ -60,7 +65,52 @@ const EXPECTED_DATA_TEMPERATURE_3 = {
     flags: 0,
     networkId: 0,
     recordNumber: 1,
-    temperature: 21.0,
+    temperatures: [ null, null, 21.0, null ],
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_VOLTAGE_2 = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    networkId: 0,
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/",
+    voltages: [ null, 12.345, null, null ]
+};
+const EXPECTED_DATA_CURRENT_4 = {
+    amperages: [ null, null, null, 123 ],
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    networkId: 0,
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_ULTRASONIC = {
+    deviceIds: [ "112233445566/3" ],
+    distance: 0.666,
+    flags: 0,
+    networkId: 0,
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_PRESSURE_1 = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    networkId: 0,
+    pressures: [ 227526.981, null ],
+    recordNumber: 1,
+    timestamp: 1645568542,
+    uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
+};
+const EXPECTED_DATA_TAMPER = {
+    deviceIds: [ "112233445566/3" ],
+    flags: 0,
+    isContactDetected: [ false ],
+    networkId: 0,
+    recordNumber: 1,
     timestamp: 1645568542,
     uri: "https://sniffypedia.org/Organization/Laird_Connectivity/"
 };
@@ -113,6 +163,36 @@ describe('lairdconnectivity', function() {
   it('should handle BT610 temperature data', function() {
     assert.deepEqual(manufacturer.process(INPUT_DATA_TEMPERATURE_3),
                      EXPECTED_DATA_TEMPERATURE_3);
+  });
+
+  // Test the process function with voltage data
+  it('should handle voltage data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_VOLTAGE_2),
+                     EXPECTED_DATA_VOLTAGE_2);
+  });
+
+  // Test the process function with current data
+  it('should handle current data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_CURRENT_4),
+                     EXPECTED_DATA_CURRENT_4);
+  });
+
+  // Test the process function with ultrasonic data
+  it('should handle ultrasonic data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_ULTRASONIC),
+                     EXPECTED_DATA_ULTRASONIC);
+  });
+
+  // Test the process function with pressure data
+  it('should handle pressure data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_PRESSURE_1),
+                     EXPECTED_DATA_PRESSURE_1);
+  });
+
+  // Test the process function with tamper data
+  it('should handle tamper data', function() {
+    assert.deepEqual(manufacturer.process(INPUT_DATA_TAMPER),
+                     EXPECTED_DATA_TAMPER);
   });
 
   // Test the process function with contact tracer data
