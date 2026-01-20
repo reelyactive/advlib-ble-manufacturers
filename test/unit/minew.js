b26211d52bcae042fba6f746eca91aa03fbd7808
jeffyactive <jeff@reelyactive.com>
2026-01-19 22:08:35 -0500

    Updated to latest standard properties

--- test/unit/minew.js
+++ test/unit/minew.js
@@ -16,22 +16,22 @@ const INPUT_DATA_S3_TEMPERATURE_HUMIDITY = 'a30364197348466745233123ac007ad3';
 const INPUT_DATA_S4_STATIC = 'a40000090600010100006745233123ac1234';
 const INPUT_DATA_S4_DOOR_ALARM = 'a40163010001ff6745233123ac1234';
 const INPUT_DATA_V3_TYPE_00 = 
-                             'ca006745233123ac6421870000000000000000001234abcd';
+                             'ca006745233123ac6421870000000000000000005a17d516';
 const INPUT_DATA_V3_TYPE_03_MBT02 = 'ca030300050005ff0421645a17d516';
 const INPUT_DATA_V3_TYPE_03_S4 = 'ca031fa01122335a17d516';
 const INPUT_DATA_V3_TYPE_05_00 =
-                             'ca05000000197348466d696e65770000000000001234abcd';
+                             'ca05000000197348466d696e65770000000000005a17d516';
 const INPUT_DATA_V3_TYPE_05_01 =
-                             'ca05010200197348466d696e65770000000100001234abcd';
+                             'ca05010200197348466d696e65770000000100005a17d516';
 const INPUT_DATA_V3_TYPE_18_00 =
-                             'ca18007f230110320000000000000000000000001234abcd';
+                             'ca18007f230110320000000000000000000000005a17d516';
 const INPUT_DATA_V3_TYPE_18_01 =
-                             'ca180106046e849b8c709a8c86987a7a8e0000001234abcd';
+                             'ca180106046e849b8c709a8c86987a7a8e0000005a17d516';
 const INPUT_DATA_V3_TYPE_1B_01 =
                              'ca1b010007000165332211ac05ef197300c5000000000010';
 const INPUT_DATA_V3_TYPE_1B_02 = 'ca1b0201016bd90e72020165332211ac030bb8';
 const INPUT_DATA_V3_TYPE_1E =
-                             'ca1e0064f4010f450700000000000000000000001234abcd';
+                             'ca1e0064f4010f450700000000000000000000005a17d516';
 
 
 // Expected outputs for the scenario
@@ -39,7 +39,7 @@ const EXPECTED_DATA_INVALID_INPUT = null;
 const EXPECTED_DATA_MSE01 = {
     batteryPercentage: 69,
     isButtonPressed: [ true, false ],
-    isContactDetected: [ true ],
+    isTamperDetected: [ false ],
     isMotionDetected: [ true ],
     levelPercentage: 0.02442002442002442,
     txCycle: 35,
@@ -48,7 +48,7 @@ const EXPECTED_DATA_MSE01 = {
 const EXPECTED_DATA_MSE02 = {
     batteryPercentage: 100,
     isButtonPressed: [ false, true ],
-    isContactDetected: [ false ],
+    isTamperDetected: [ true ],
     isMotionDetected: [ false ],
     levelPercentage: 100,
     txCycle: 70,
@@ -56,8 +56,9 @@ const EXPECTED_DATA_MSE02 = {
 };
 const EXPECTED_DATA_S3_STATIC = {
     deviceIds: [ 'ac2331234567/2' ],
-    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/",
-    version: "1.0.23"
+    firmwareVersion: "1.0.23",
+    hardwareVersion: "11",
+    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_S3_TEMPERATURE_HUMIDITY = {
     batteryPercentage: 100,
@@ -68,20 +69,23 @@ const EXPECTED_DATA_S3_TEMPERATURE_HUMIDITY = {
 };
 const EXPECTED_DATA_S4_STATIC = {
     deviceIds: [ 'ac2331234567/2' ],
-    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/",
-    version: "0.9.6"
+    firmwareVersion: "0.9.6",
+    hardwareVersion: "1",
+    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_S4_DOOR_ALARM = {
     batteryPercentage: 99,
     deviceIds: [ 'ac2331234567/2' ],
     isContactDetected: [ false ],
+    isTamperDetected: [ true ],
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_00 = {
     batteryPercentage: 100,
     deviceIds: [ 'ac2331234567/2' ],
-    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/",
-    version: "1.3.7"
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
+    firmwareVersion: "1.3.7",
+    uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_03_MBT02 = {
     acceleration: [ 0.01953125, 0.01953125, -0.984375 ],
@@ -90,31 +94,35 @@ const EXPECTED_DATA_V3_TYPE_03_MBT02 = {
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_03_S4 = {
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     isContactDetected: [ false ],
     isContactDetectedCycle: 17,
     isTamperDetected: [ false ],
     isTamperDetectedCycle: 51,
-    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_05_00 = {
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     name: "minew",
     temperature: 25.44921875,
     relativeHumidity: 72.2734375,
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_05_01 = {
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     isMarked: [ true ],
     name: "minew",
     temperature: 25.44921875,
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_18_00 = {
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     passageCounts: [ 0x0123, 0x3210 ],
     passageCountsCycle: 127,
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 const EXPECTED_DATA_V3_TYPE_18_01 = {
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     numberOfOccupants: 4,
     numberOfOccupantsCycle: 6,
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
@@ -136,12 +144,13 @@ const EXPECTED_DATA_V3_TYPE_1B_02 = {
 const EXPECTED_DATA_V3_TYPE_1E = {
     distance: 0.5,
     distanceCycle: 100,
+    encrypted: { salt: 23063, checksum: 54550, method: "minew-connect-v3" },
     isBatteryLow: true,
-    isContactDetected: [ false ],
+    isTamperDetected: [ true ],
     isMotionDetected: [ true ],
     isOccupancyDetected: [ true ],
-    occupancyDetectionCycle: 69,
-    tamperCycle: 7,
+    isOccupancyDetectedCycle: 69,
+    isTamperDetectedCycle: 7,
     uri: "https://sniffypedia.org/Organization/Shenzhen_Minew_Technologies_Co_Ltd/"
 };
 
